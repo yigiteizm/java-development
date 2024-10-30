093d43f22583e85bf8bffd9f5e5c56dc8db031e8
Yigit <yigistaff@gmail.com>
2024-10-30 10:28:54 -0400

    Cash class has been added to project.

--- Workbook-5/AssetManager/src/main/java/com/pluralsight/Cash.java
+++ Workbook-5/AssetManager/src/main/java/com/pluralsight/Cash.java
@@ -1,6 +1,23 @@
 package com.pluralsight;
 
-public class Cash {
+public class Cash extends Asset{
+    private double numberOfTimesStashedTheCash;
 
+    public Cash(String description, String dateAcquired, double originalCost, double numberOfTimesStashedTheCash) {
+        super(description, dateAcquired, originalCost);
+        this.numberOfTimesStashedTheCash = numberOfTimesStashedTheCash;
+    }
 
+    public double getNumberOfTimesStashedTheCash() {
+        return numberOfTimesStashedTheCash;
+    }
+
+    public void setNumberOfTimesStashedTheCash(double numberOfTimesStashedTheCash) {
+        this.numberOfTimesStashedTheCash = numberOfTimesStashedTheCash;
+    }
+
+    @Override
+    public double getValue() {
+        return getOriginalCost();
+    }
 }
