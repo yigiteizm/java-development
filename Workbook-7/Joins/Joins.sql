82ffa3661350c27071b7e7c9dcc5274c9a71972c
Yigit <yigistaff@gmail.com>
2024-11-21 11:43:00 -0500

    question 4,5,6 has been updated according to Raymond's solution.

--- Workbook-7/Joins/Joins.sql
+++ Workbook-7/Joins/Joins.sql
@@ -1,6 +1,4 @@
 -- Question 1: List the product id, product name, unit price, and category name of all products.
-
-
 SELECT 
     Products.ProductID, Products.ProductName, Products.UnitPrice, Categories.CategoryName
 FROM Products
@@ -9,7 +7,6 @@ ORDER BY
     Categories.CategoryName, 
     Products.ProductName;
 -- Question 2: List the product id, product name, unit price, and supplier name of all products that cost more than $75.
-
 SELECT 
     Products.ProductID, Products.ProductName, Products.UnitPrice, Suppliers.CompanyName AS SupplierName
 FROM Products
@@ -19,8 +16,6 @@ WHERE
 ORDER BY 
     Products.ProductName;
 -- Question 3: List the product id, product name, unit price, category name, and supplier name of every product.
-
-
 SELECT 
     Products.ProductID, Products.ProductName, Products.UnitPrice, Categories.CategoryName, Suppliers.CompanyName AS SupplierName
 FROM Products
@@ -28,33 +23,24 @@ JOIN Categories ON Products.CategoryID = Categories.CategoryID
 JOIN Suppliers ON Products.SupplierID = Suppliers.SupplierID
 ORDER BY 
     Products.ProductName;
--- Question 4: What is the product name(s) and categories of the most expensive products?
+-- 4. What is the product name(s) and categories of the most expensive products? HINT: Find the max price in a subquery and then use that in your more complex query that joins products with categories.
+SELECT p.ProductName, c.CategoryName
+FROM Products p
+INNER JOIN Categories c ON p.CategoryID = c.CategoryID
+WHERE p.UnitPrice = (
+  SELECT MAX(UnitPrice)
+  FROM Products
+);
 
+-- 5. List the order id, ship name, ship address, and shipping company name of every order that shipped to Germany.
+SELECT o.OrderID, o.ShipName, o.ShipAddress, s.CompanyName AS ShippingCompany
+FROM Orders o
+INNER JOIN Shippers s ON o.ShipVia = s.ShipperID
+WHERE o.ShipCountry = 'Germany';
 
-SELECT 
-    Products.ProductName,  Categories.CategoryName, Products.UnitPrice
-FROM Products
-JOIN Categories ON Products.CategoryID = Categories.CategoryID
-WHERE 
-    Products.UnitPrice = (SELECT MAX(UnitPrice) FROM Products);
--- Question 5: List the order id, ship name, ship address, and shipping company name of every order that shipped to Germany.
-
-
-SELECT 
-    Orders.OrderID, Orders.ShipName, Orders.ShipAddress, Shippers.CompanyName AS ShippingCompanyName
-FROM Orders
-JOIN Shippers ON Orders.ShipVia = Shippers.ShipperID
-WHERE 
-    Orders.ShipCountry = 'Germany';
--- Question 6: List the order id, order date, ship name, and ship address of all orders that ordered "Sasquatch Ale".
-
-SELECT 
-    Orders.OrderID, 
-    Orders.OrderDate, 
-    Orders.ShipName, 
-    Orders.ShipAddress
-FROM Orders
-JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID
-JOIN Products ON OrderDetails.ProductID = Products.ProductID
-WHERE 
-    Products.ProductName = 'Sasquatch Ale';
+-- 6. List the order id, order date, ship name, ship address of all orders that ordered "Sasquatch Ale".
+SELECT o.OrderID, o.OrderDate, o.ShipName, o.ShipAddress
+FROM Orders o
+INNER JOIN `Order Details` od ON o.OrderID = od.OrderID
+INNER JOIN Products p ON od.ProductID = p.ProductID
+WHERE p.ProductName = 'Sasquatch Ale';
